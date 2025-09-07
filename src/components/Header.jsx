2470d262645a8e607eae74cb5bd88a2170309a22
bugtok <davemarapao@gmail.com>
2025-09-08 04:21:37 +0800

    Revert "print fix"
    This reverts commit 0c3c84b5798e71335df3988e0376749d69aa1efe.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ isEditable, setIsEditable }) => {
   return (
-    <header className="fixed top-0 left-0 w-full flex justify-between items-center py-3 px-4 sm:py-4 sm:px-6 bg-white shadow-md z-50 border-b border-gray-100 no-print">
+    <header className="fixed top-0 left-0 w-full flex justify-between items-center py-3 px-4 sm:py-4 sm:px-6 bg-white shadow-md z-50 border-b border-gray-100">
       <h1 className="text-xl sm:text-2xl font-bold text-gray-800 flex items-center">
         <svg className="w-5 h-5 sm:w-6 sm:h-6 mr-2 text-blue-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd"></path>
