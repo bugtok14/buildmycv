21921b9b1f09b13d98f4a09ce272d0b60e2c6757
bugtok <davemarapao@gmail.com>
2025-09-12 01:37:12 +0800

    brand

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = ({ isEditable, setIsEditable, isGeneratingPDF }) => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
           </svg>
           <h1 className="text-2xl font-bold bg-clip-text text-transparent bg-gray-800">
-            Resume Builder
+            BuildMyCV
           </h1>
         </div>
         
